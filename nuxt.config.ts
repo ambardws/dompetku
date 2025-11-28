0454eee87137bd9d57f77ef282b70c51064320ff
ambardws <ambardwisaputra@gmail.com>
2025-11-28 16:28:07 +0700

    feat: add icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,7 +69,10 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Personal finance tracker PWA' }
+        { name: 'description', content: 'Personal finance tracker PWA' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
       ]
     }
   },
