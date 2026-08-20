7f2789d1255bea9bf27af469cc9201af4b944168
ambardws <ambardwisaputra@gmail.com>
2026-08-20 10:39:15 +0700

    ui: minimalize recurring button and add Budgets to nav
    - Make recurring transactions link minimalist inline button
    - Replace Categories with Budgets in bottom nav (5 items: Home, Transactions, Budgets, Reminders, Insights)
    - Categories accessible via ActionsMenu in header

--- src/shared/ui/organisms/DBottomNav.vue
+++ src/shared/ui/organisms/DBottomNav.vue
@@ -71,9 +71,9 @@ const navItems = [
     iconPath: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01'
   },
   {
-    path: '/categories',
-    label: 'Categories',
-    iconPath: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z'
+    path: '/budgets',
+    label: 'Budgets',
+    iconPath: 'M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z'
   },
   {
     path: '/reminders',
